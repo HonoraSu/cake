af0f276b4522f138477a0e804accf0a40600e5f8
HonoraSu <1914613553@qq.com>
2019-05-25 20:57:47 +0800

    cake

---
+++ service/users.js
@@ -0,0 +1,2 @@
+//模拟数据库
+module.exports=[{name:'wcs',password:123456},{name:'yw',password:123456}];
--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,44 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div id="app">
+        <!--<Header></Header>-->
+        <router-view></router-view>
+        <!--<Footer></Footer>-->
     </div>
-    <router-view/>
-  </div>
 </template>
 
-<style lang="less">
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
+<script>
+    import Header from './base/Header'
+    import Footer from './base/Footer'
+    import More from './components/more'
+
+    export default {
+        name: "App",
+        data() {
+            return {}
+        },
+        methods: {},
+        components: {
+            Header,
+            Footer,
+            More
+        },
+        watch: {},
+        computed: {},
     }
-  }
-}
+</script>
+<style lang="less">
+    * {
+        padding: 0;
+        margin: 0;
+    }
+    ul li {
+        list-style: none
+    }
+    a {
+        text-decoration: none
+    }
+    h3 {
+        color: #482615;
+        font-size: .15*2rem;
+    }
 </style>
